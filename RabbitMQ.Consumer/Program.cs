a84d9e06be5c447b94bfaf5f1d84134df717cd00
İhsan Efe Uzun <139785527+ihsanefeuzun@users.noreply.github.com>
2024-12-22 20:38:59 +0300

    Add files via upload

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Consumer.Service;
+using System;
+
+namespace RabbitMQ.Consumer
+{
+    public class Program
+    {
+        private static string connectionString = "amqp://guest:guest@localhost:5672";
+
+        static void Main(string[] args)
+        {
+            var queueName = args.Length > 0 ? args[0] : "notification_queue";
+
+            using var connection = GetConnection();
+            var consumerService = new RabbitMqConsumerService(connection);
+
+            // Start consuming messages
+            consumerService.StartConsuming();
+
+            Console.WriteLine($"{queueName} listening....\nPress Enter to exit.");
+
+            // Wait for user input to stop consuming
+            Console.ReadLine();
+
+            // Cleanly stop consuming
+            consumerService.StopConsuming();
+        }
+
+        private static IConnection GetConnection()
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri(connectionString, UriKind.RelativeOrAbsolute)
+            };
+
+            return factory.CreateConnection();
+        }
+    }
+}
---
+++ RabbitMQ.Consumer/Service/RabbitMqConsumerService.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Consumer.Service
+{
+    public class RabbitMqConsumerService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private string _queueName = "notification_queue";
+
+        public RabbitMqConsumerService(IConnection connection)
+        {
+            _connection = connection;
+            _channel = _connection.CreateModel();
+
+            // Queue ve Exchange yapılandırması
+            _channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            _channel.QueueBind(queue: _queueName, exchange: "notification_exchange", routingKey: "notification_routing_key");
+        }
+
+        public void StartConsuming()
+        {
+            var consumerEvent = new EventingBasicConsumer(_channel);
+
+            consumerEvent.Received += (ch, e) =>
+            {
+                var byteArr = e.Body.ToArray();
+                var bodyStr = Encoding.UTF8.GetString(byteArr);
+
+                
+                Console.WriteLine($"Received Data: {bodyStr}");
+
+                
+                _channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(queue: _queueName, autoAck: false, consumer: consumerEvent);
+
+            Console.WriteLine($"{_queueName} listening....\n\n\n");
+        }
+
+        public void StopConsuming()
+        {
+           
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+            }
+            if (_connection.IsOpen)
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
